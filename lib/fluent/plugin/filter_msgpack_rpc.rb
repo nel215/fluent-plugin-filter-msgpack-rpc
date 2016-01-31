19b633b8483baea01ce058f3c773f654ac1cc249
nel215 <otomo.yuhei@gmail.com>
2016-01-31 23:00:19 +0900

    added simple client and debug logger

--- lib/fluent/plugin/filter_msgpack_rpc.rb
+++ lib/fluent/plugin/filter_msgpack_rpc.rb
@@ -3,18 +3,27 @@ module Fluent
     Fluent::Plugin.register_filter('msgpack_rpc', self)
 
     def configure(conf)
+      log.debug "enter configure"
       super
+
+      require "msgpack/rpc"
     end
 
     def start
+      log.debug "enter start"
       super
+
+      @client = MessagePack::RPC::Client.new('localhost', 3000)
     end
 
     def shutdown
+      log.debug "enter shutdown"
       super
     end
 
     def filter(tag, time, record)
+      log.debug "enter filter"
+      @client.call(:method, time, record)
       record
     end
   end
